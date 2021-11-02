9657cb1b7fcce3479850a5b9430b0953698c5a93
aakash <spysky1998@gmail.com>
2021-11-02 15:21:42 -0700

    aa

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const router = require('./router');
 const cors = require('cors');
 
-const port = 8000 || process.env.PORT
+const port = process.env.PORT || 5000;
 const host = '0.0.0.0'; 
 
 const app = express();
